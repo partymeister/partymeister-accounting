9c589f9fa6538c891f36ebd49573b0dd1861f3f7
Reza Esmaili <github@dfox.info>
2018-03-28 12:02:57 +0200

    Fix permission problem

--- src/Grids/AccountGrid.php
+++ src/Grids/AccountGrid.php
@@ -20,8 +20,8 @@ class AccountGrid extends Grid
         $this->addColumn('currency_iso_4217', trans('partymeister-accounting::backend/accounts.currency_iso_4217'));
         $this->addColumn('last_booking', trans('partymeister-accounting::backend/accounts.last_booking'))->renderer(DateRenderer::class);
         $this->addColumn('balance', trans('partymeister-accounting::backend/accounts.balance'))->renderer(CurrencyRenderer::class, ['currency_column' => 'currency_iso_4217'])->style('text-align: right');
-        $this->addAction(trans('partymeister-accounting::backend/accounts.show_pos'), 'backend.pos.show', ['class' => 'btn-primary'])->needsPermissionTo('backend.bookings.create')->onCondition('has_pos', true);
-        $this->addAction(trans('partymeister-accounting::backend/accounts.edit_pos'), 'backend.pos.edit', ['class' => 'btn-warning'])->needsPermissionTo('backend.bookings.create')->onCondition('has_pos', true);
+        $this->addAction(trans('partymeister-accounting::backend/accounts.show_pos'), 'backend.pos.show', ['class' => 'btn-primary'])->needsPermissionTo('backend.bookings.write')->onCondition('has_pos', true);
+        $this->addAction(trans('partymeister-accounting::backend/accounts.edit_pos'), 'backend.pos.edit', ['class' => 'btn-warning'])->needsPermissionTo('backend.bookings.write')->onCondition('has_pos', true);
         $this->addEditAction(trans('motor-backend::backend/global.edit'), 'backend.accounts.edit');
         $this->addDeleteAction(trans('motor-backend::backend/global.delete'), 'backend.accounts.destroy');
 

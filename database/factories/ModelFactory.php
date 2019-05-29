98d0bba3e01732b9f0ff78cadb55932e7c31a98c
Reza Esmaili <github@dfox.info>
2019-05-29 12:28:34 +0200

    Replace deprecated helpers with their class methods

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -1,8 +1,10 @@
 <?php
 
+use Illuminate\Support\Str;
+
 $factory->define(Partymeister\Accounting\Models\AccountType::class, function (Faker\Generator $faker) {
     return [
-        'name'       => $faker->word . '.' . str_random(20),
+        'name'       => $faker->word . '.' . Str::random(20),
         'created_by' => factory(Motor\Backend\Models\User::class)->create()->id,
         'updated_by' => factory(Motor\Backend\Models\User::class)->create()->id,
     ];
@@ -10,7 +12,7 @@ $factory->define(Partymeister\Accounting\Models\AccountType::class, function (Fa
 
 $factory->define(Partymeister\Accounting\Models\Account::class, function (Faker\Generator $faker) {
     return [
-        'name'              => $faker->word . '.' . str_random(20),
+        'name'              => $faker->word . '.' . Str::random(20),
         'account_type_id'   => factory(Partymeister\Accounting\Models\AccountType::class)->create()->id,
         'has_pos'           => (bool) rand(0, 1),
         'currency_iso_4217' => 'EUR',

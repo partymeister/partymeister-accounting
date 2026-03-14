bf528cd401ea1a77db7077e2f6439436acc8e663
Reza Esmaili <github@dfox.info>
2026-03-14 16:46:20 +0100

    UX audit fixes: accessibility, touch targets, consistency, mobile

--- resources/views/frontend/components/item-list-tw.blade.php
+++ resources/views/frontend/components/item-list-tw.blade.php
@@ -1,5 +1,6 @@
 @foreach ($itemTypes as $itemType)
-    <h4>{{$itemType->name}}</h4>
+    <div class="mb-8 last:mb-0">
+    <h4 class="mb-2">{{$itemType->name}}</h4>
     @if ($itemType->items->count() > 0)
         <div class="overflow-x-auto">
             <table class="w-full text-left text-sm">
@@ -16,7 +17,7 @@
                             {{$item->name}}
                             @if (strlen($item->description) > 2)
                                 <br>
-                                <small class="text-sm opacity-70">{{$item->description}}</small>
+                                <small class="text-sm text-text-muted">{{$item->description}}</small>
                             @endif
                         </td>
                         <td class="px-4 py-3 border-t border-border text-text text-right">
@@ -28,4 +29,5 @@
             </table>
         </div>
     @endif
+    </div>
 @endforeach
